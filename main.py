1cfdf8986770cfaad84c719217b0f5c8513bf2f4
pandolapapas <pandolapapas@gmail.com>
2022-03-12 18:19:25 +0900

    1st commit

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 st.title('Streamlit 入門')
+
+df = pd.DataFrame(
+    np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
+    columns=['lat', 'lon']
+)
+st.map(df)
